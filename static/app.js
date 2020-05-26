312f1d4aa14788cb1b5138bb894465b1a93ac176
Dru Serkes <DruSerkes@gmail.com>
2020-05-26 10:43:46 -0400

    Finish OOP refactoring

--- static/app.js
+++ static/app.js
@@ -10,15 +10,120 @@ const $highscore = $('.highscore');
 const $gamesPlayed = $('.games-played');
 const $playAgain = $('.play-again');
 
-let currentScore = 0;
-let words = [];
+// let currentScore = 0;
+// let words = [];
 
-$start.on('click', playGame);
+class BoggleGame {
+	constructor() {
+		this.score = 0;
+		this.words = new Set();
+		this.time = 60;
+		this.playGame();
+	}
+
+	updateScore() {
+		$score.text(this.score);
+	}
+
+	hideGame() {
+		$gameContainer.addClass('hidden');
+		$playAgain.removeClass('hidden');
+	}
+
+	showGame() {
+		$gameContainer.removeClass('hidden');
+		$start.addClass('hidden');
+		if (!$playAgain.hasClass('hidden')) {
+			$playAgain.addClass('hidden');
+		}
+	}
+
+	updateRecord() {
+		$highscore.text(this.score);
+		this.displayMessage('Congrats - New High Score!');
+	}
+
+	runTimer() {
+		let gameTimer = setInterval(() => {
+			$timer.text(this.time);
+			if (this.time === 0) {
+				clearInterval(gameTimer);
+				$timer.text(this.time);
+				this.hideGame();
+				this.gameOver();
+			}
+			this.time -= 1;
+		}, 1000);
+	}
+
+	displayMessage(message) {
+		$message.empty();
+		$message.text(message);
+		$guess.val('').focus();
+	}
+
+	async gameOver() {
+		// send score as JSON to '/game-over'
+		const response = await axios.post('/game-over', { score: this.score });
+
+		// update DOM with response
+		response.data.newRecord
+			? this.updateRecord()
+			: this.displayMessage('Good Game - Try again!');
+	}
+
+	async playGame(e) {
+		this.showGame();
+		$form.on('submit', this.handleSubmit.bind(this));
+		this.runTimer.bind(this)();
+	}
+
+	async handleSubmit(e) {
+		e.preventDefault();
+
+		// get value from input field
+		const word = $guess.val();
+		if (!word) return;
+
+		if (this.words.has(word)) {
+			this.displayMessage("You've already guessed that word");
+			return;
+		}
+
+		// save the response
+		const response = await axios.get('/guess', {
+			params : {
+				guess : word
+			}
+		});
+
+		// Handle result
+		let result = response.data.result;
+
+		if (result === 'not-word') {
+			this.displayMessage('Sorry, that is not a word in our dictionary');
+		} else if (result === 'not-on-board') {
+			this.displayMessage('Sorry, that word is not on the board!');
+		} else {
+			this.words.add(word);
+			this.displayMessage('You got a word!');
+			this.score += word.length;
+			this.updateScore();
+		}
+		// this.displayMessage('Sorry, that word is not on the board!');
+	}
+}
+
+$start.on('click', function() {
+	boggle = new BoggleGame();
+});
 
 $playAgain.on('click', function() {
 	location.reload();
 });
 
+// commented out to test refactored OOP
+/*
 async function handleSubmit(event) {
 	event.preventDefault();
 
@@ -117,3 +222,4 @@ const updateScore = (score) => {
 	currentScore += score;
 	$score.text(`${currentScore}`);
 };
+*/
